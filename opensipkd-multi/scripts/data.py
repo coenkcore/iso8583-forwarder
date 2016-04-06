7e905fe456175d648e3a3901ccd5f608f2d05b02
aagusti <aa.gustiana@gmail.com>
2016-04-06 20:14:37 +0700

    scripts

---
+++ opensipkd-multi/scripts/data.py
@@ -0,0 +1,29 @@
+data = [
+    ('pad_channel', [
+        {'id': 6010, 'nama': 'Teller'},
+        {'id': 6011, 'nama': 'ATM'},
+        {'id': 6012, 'nama': 'POS'},
+        {'id': 6013, 'nama': 'Phone Banking'},
+        {'id': 6014, 'nama': 'Internet Banking'},
+        {'id': 6015, 'nama': 'Kiosk'},
+        {'id': 6016, 'nama': 'Autodebet'},
+        {'id': 6017, 'nama': 'Mobil Banking'},
+        ]),
+    ('pad_tp', [
+        {'id': 2,
+         'singkatan': 'BRI',
+         'nama': 'Bank Rakyat Indonesia'},
+        {'id': 9,
+         'singkatan': 'BNI',
+         'nama': 'Bank Negara Indonesia'},
+        {'id': 110,
+         'singkatan': 'BJB',
+         'nama': 'Bank Jabar Banten'},
+        {'id': 200,
+         'singkatan': 'BTN',
+         'nama': 'Bank Tabungan Negara'},
+        {'id': 700,
+         'singkatan': 'POS',
+         'nama': 'PT Pos Indonesia'},
+        ])
+    ]
---
+++ opensipkd-multi/scripts/setup.py
@@ -0,0 +1,32 @@
+import os
+import sys
+import imp
+from sqlalchemy import create_engine
+sys.path[0:0] = ['/etc/opensipkd']
+from multi_conf import db_url
+sys.path[0:0] = ['/usr/share/opensipkd/modules']
+from fixtures import insert
+from base_models import Base
+data_dir = os.path.split(__file__)[0]
+data_file = os.path.join(data_dir, 'data.py')
+data_module = imp.load_source('data', data_file)
+data = data_module.data
+
+
+def init():
+    if not db_url:
+        print('Sesuaikan db_url pada /etc/opensipkd/multi_conf.py')
+        return
+    data_dir = os.path.split(__file__)[0]
+    data_file = os.path.join(data_dir, 'data.py')
+    data_module = imp.load_source('data', data_file)
+    data = data_module.data
+    engine = create_engine(db_url)
+    Base.metadata.bind = engine
+    sys.path[0:0] = ['/usr/share/opensipkd-forwarder/modules/multi']
+    from log_models import (
+        Payment,
+        Reversal,
+        )
+    Base.metadata.create_all()
+    #nsert(engine, data)
