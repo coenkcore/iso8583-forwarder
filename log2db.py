1eb59f0a9fe0ac87b9a66758e129632a00728f98
Owo Sugiana <sugiana@opensipkd.com>
2018-06-08 14:39:08 +0700

    log2db kini bisa tail

--- log2db.py
+++ log2db.py
@@ -1,16 +1,28 @@
 # Gunakan Python 3
+import os
 import sys
 import re
+import pathmagic
+import select
 from hashlib import md5
 from sqlalchemy import create_engine
 from sqlalchemy.exc import IntegrityError
-import pathmagic
+from optparse import OptionParser
+from time import sleep
+from subprocess import (
+    Popen,
+    PIPE,
+    )
 from tools import create_datetime
 from base_models import (
     Base,
     DBSession,
     )
-from log.models import Log
+from demon import make_pid
+from log.models import (
+    Log,
+    Jenis,
+    )
 from log.conf import db_url
 
 
@@ -35,35 +47,83 @@ def save():
     DBSession.commit()
 
 
-KATEGORI = (None, 'INFO', 'ERROR', 'WARNING')
-REGEX_FIRST_LINE = '^([\d]*)-([\d]*)-([\d]*) ([\d]*):([\d]*):([\d]*),([\d]*) '\
-        '(INFO|ERROR|WARNING)'
-REGEX_FIRST_LINE = re.compile(REGEX_FIRST_LINE)
-
-jenis_id = int(sys.argv[1])
-log_file = sys.argv[2]
-
 engine = create_engine(db_url)
 Base.metadata.bind = engine
 DBSession.configure(bind=engine)
 
 lines = []
-line_id = None
-f = open(log_file)
-for line in f.readlines():
-    s = line.rstrip()
-    match = REGEX_FIRST_LINE.search(s)
-    if match:
-        if line_id:
-            save()
-        line_id = md5(s.encode('utf-8')).hexdigest()
-        lines = [s]
-        year, month, day, hour, minute, sec, msec, kategori = match.groups()
-        waktu = create_datetime(
+q = DBSession.query(Jenis).order_by(Jenis.id)
+for row in q:
+    line = '{}: {}'.format(row.id, row.nama)
+    lines.append(line)
+help_jenis = ', '.join(lines)
+
+default_pid_file = 'log2db.pid'
+
+pars = OptionParser()
+pars.add_option('-j', '--jenis', help=help_jenis)
+pars.add_option('-f', '--file', help='log file')
+pars.add_option('-t', '--tail-mode-only', action='store_true')
+pars.add_option(
+    '', '--pid-file', default=default_pid_file,
+    help='default '+default_pid_file)
+option, remain = pars.parse_args(sys.argv[1:])
+
+pid_file = os.path.realpath(option.pid_file)
+pid = make_pid(pid_file)
+
+KATEGORI = (None, 'INFO', 'ERROR', 'WARNING')
+REGEX_FIRST_LINE = '^([\d]*)-([\d]*)-([\d]*) ([\d]*):([\d]*):([\d]*),([\d]*) '\
+        '(INFO|ERROR|WARNING)'
+REGEX_FIRST_LINE = re.compile(REGEX_FIRST_LINE)
+
+jenis_id = int(option.jenis)
+log_file = option.file
+
+if not option.tail_mode_only:
+    lines = []
+    line_id = None
+    f = open(log_file)
+    for line in f.readlines():
+        s = line.rstrip()
+        match = REGEX_FIRST_LINE.search(s)
+        if match:
+            if line_id:
+                save()
+            line_id = md5(s.encode('utf-8')).hexdigest()
+            lines = [s]
+            year, month, day, hour, minute, sec, msec, kategori = \
+                match.groups()
+            waktu = create_datetime(
                     int(year), int(month), int(day), int(hour),
                     int(minute), int(sec), int(msec)*1000)
-    else:
-        lines.append(s)
-f.close()
-if line_id:
-    save()
+        else:
+            lines.append(s)
+    f.close()
+    if line_id:
+        save()
+
+lines = []
+line_id = None
+f = Popen(['tail', '-f', log_file], stdout=PIPE, stderr=PIPE)
+p = select.poll()
+p.register(f.stdout)
+while True:
+    if p.poll(1):
+        line = f.stdout.readline()
+        line = line.rstrip()
+        line = line.decode('utf-8')
+        match = REGEX_FIRST_LINE.search(line)
+        if match:
+            if line_id:
+                save()
+            line_id = md5(line.encode('utf-8')).hexdigest()
+            lines = [line]
+            year, month, day, hour, minute, sec, msec, kategori = \
+                match.groups()
+            waktu = create_datetime(
+                    int(year), int(month), int(day), int(hour),
+                    int(minute), int(sec), int(msec)*1000)
+        else:
+            lines.append(line)
+    sleep(0.1)
